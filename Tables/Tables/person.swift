e3297c304ef86b79bb6428b42cc502bf81fb47c9
Illya-Gurkov <109282408+Illya-Gurkov@users.noreply.github.com>
2022-08-15 00:02:44 +0300

    commitOne

---
+++ Tables/Tables/person.swift
@@ -0,0 +1,15 @@
+//
+//  person.swift
+//  Tables
+//
+//  Created by Illya Gurkov on 14.08.22.
+//
+
+import Foundation
+
+struct person {
+    let fulname: String
+    let emali : String
+    let phoneNumber: String
+}
+
